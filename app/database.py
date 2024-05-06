fe37c89787a52ab2b638213f85db54ffbdac375e
zoebrito <zb72@njit.edu>
2024-05-06 15:23:21 -0400

    Refactored for new database

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
+# database.py
+
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
+from app.analytics_models import Base as AnalyticsBase  # Import the Base class from analytics_models.py
 
 Base = declarative_base()
+Base.metadata.schema = "your_schema_name"  # Set the schema if needed
 
 class Database:
     """Handles database connections and sessions."""
@@ -16,6 +20,10 @@ class Database:
             cls._session_factory = sessionmaker(
                 bind=cls._engine, class_=AsyncSession, expire_on_commit=False, future=True
             )
+
+            # Create all tables defined in models
+            Base.metadata.create_all(cls._engine)
+            AnalyticsBase.metadata.create_all(cls._engine)  # Create the UserConversion table
 
     @classmethod
     def get_session_factory(cls):
