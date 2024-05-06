1a6ba8220aa3302d21df1d20eff3a73fde6942c2
zoebrito <zb72@njit.edu>
2024-05-06 18:26:15 -0400

    Restore to working condition

--- app/database.py
+++ app/database.py
@@ -2,10 +2,8 @@
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
-from app.analytics_models import Base as AnalyticsBase  # Import the Base class from analytics_models.py
 
 Base = declarative_base()
-Base.metadata.schema = "your_schema_name"  # Set the schema if needed
 
 class Database:
     """Handles database connections and sessions."""
@@ -20,10 +18,6 @@ class Database:
             cls._session_factory = sessionmaker(
                 bind=cls._engine, class_=AsyncSession, expire_on_commit=False, future=True
             )
-
-            # Create all tables defined in models
-            Base.metadata.create_all(cls._engine)
-            AnalyticsBase.metadata.create_all(cls._engine)  # Create the UserConversion table
 
     @classmethod
     def get_session_factory(cls):
